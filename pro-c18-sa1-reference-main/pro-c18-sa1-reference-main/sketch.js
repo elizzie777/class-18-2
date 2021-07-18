851a53a84af0bb87779ca0977282693ca7c3d6b5
elizzie777 <85830044+elizzie777@users.noreply.github.com>
2021-07-18 18:54:52 +0100

    Add files via upload

---
+++ pro-c18-sa1-reference-main/pro-c18-sa1-reference-main/sketch.js
@@ -0,0 +1,19 @@
+var s1
+var s2
+
+function setup() 
+{
+  createCanvas(400, 400);
+  s1 = new Student("john",12,6);
+  s2 = new Student("lucy",10,5)
+  s1.display()
+  s2.display()
+
+}
+function draw() 
+{
+  background(51);
+ 
+}
+
+
